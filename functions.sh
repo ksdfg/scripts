3600db8ab33c975c79822306e9bb9aff09da846f
Akhil Narang <me@akhilnarang.dev>
2020-10-07 14:52:30 +0530

    scripts: functions: format and fix shellcheck issues
     * shfmt -w -s -ci -sr -i 4 functions.sh
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- functions.sh
+++ functions.sh
@@ -2,42 +2,42 @@
 
 # update everything
 update() {
-	# update icon pack
-	echo "----- updating icon theme -----"
-	cd $HOME/.icons/candy-icons
-	git fetch
-	git merge origin master --strategy-option=theirs
-	cd - > /dev/null
+    # update icon pack
+    echo "----- updating icon theme -----"
+    cd "$HOME"/.icons/candy-icons || exit
+    git fetch
+    git merge origin master --strategy-option=theirs
+    cd - > /dev/null || exit
 
-	# update heroku cli
-	echo "\n--- updating heroku cli ---"
-	heroku update
+    # update heroku cli
+    echo "\n--- updating heroku cli ---"
+    heroku update
 
-	# update libraries
-	echo "\n----- updating libraries -----"
-	if [[ "$1" == "-m" ]]; then
-		sudo echo "Updating mirror list may take a while ..."    # sudo here just so that it asks for pwd before showing this string
-		sudo reflector --latest 200 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-	fi
-	yay
+    # update libraries
+    echo "\n----- updating libraries -----"
+    if [[ $1 == "-m" ]]; then
+        sudo echo "Updating mirror list may take a while ..." # sudo here just so that it asks for pwd before showing this string
+        sudo reflector --latest 200 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+    fi
+    yay
 
-	# update scripts upstream
-	echo "\n----- updating scripts upstream -----"
-	cp $HOME/.zshrc $HOME/scripts/.zshrc
-	cp $HOME/.gitconfig $HOME/scripts/.gitconfig
-	yay -Qq > $HOME/scripts/packages.txt
-	cd $HOME/scripts
-	git c -am "automated update"
-	git p
-	cd - > /dev/null
+    # update scripts upstream
+    echo "\n----- updating scripts upstream -----"
+    cp "$HOME"/.zshrc "$HOME"/scripts/.zshrc
+    cp "$HOME"/.gitconfig "$HOME"/scripts/.gitconfig
+    yay -Qq > "$HOME"/scripts/packages.txt
+    cd "$HOME"/scripts || exit
+    git c -am "automated update"
+    git p
+    cd - > /dev/null || exit
 }
 
 # update stuff and get lost
 bbye() {
-	update $* && poweroff
+    update "$*" && poweroff
 }
 
 # update stuff and reboot
 rebye() {
-	update $* && reboot
+    update "$*" && reboot
 }
