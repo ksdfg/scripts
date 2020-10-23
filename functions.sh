a6a954651edc85cdc4c80216f7a59599d9e82cdf
ksdfg <ksdfg123@gmail.com>
2020-10-23 18:16:42 +0530

    add function to reset gcloud login and config cuz cloud shell never stops provisioning smh
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- functions.sh
+++ functions.sh
@@ -9,7 +9,7 @@ update() {
     cd "$HOME"/.icons/candy-icons || exit
     git fetch
     git merge origin master --strategy-option=theirs
-    cd - > /dev/null || exit
+    cd - >/dev/null || exit
 
     # update heroku cli
     echo "\n--- updating heroku cli ---"
@@ -27,11 +27,11 @@ update() {
     echo "\n----- updating scripts upstream -----"
     cp "$HOME"/.zshrc "$HOME"/scripts/.zshrc
     cp "$HOME"/.gitconfig "$HOME"/scripts/.gitconfig
-    yay -Qq > "$HOME"/scripts/packages.txt
+    yay -Qq >"$HOME"/scripts/packages.txt
     cd "$HOME"/scripts || exit
     git c -am "automated update"
     git p
-    cd - > /dev/null || exit
+    cd - >/dev/null || exit
 }
 
 # update stuff and get lost
@@ -42,4 +42,13 @@ bbye() {
 # update stuff and reboot
 rebye() {
     update "$*" && reboot
+}
+
+# reset gcloud account and config
+gcloud-reset() {
+    for email in $(gcloud auth list --filter "account!=ksdfg123@gmail.com" --format "value(account)"); do
+        gcloud auth revoke $email
+    done
+    gcloud config set account ksdfg123@gmail.com
+    gcloud config set project madness-sense
 }
