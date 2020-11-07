cbb310f7c48593c334fa88e244940611e38f5d0d
ksdfg <ksdfg123@gmail.com>
2020-11-08 00:25:23 +0530

    setup p10k and oh-my-zsh on install
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- setup.sh
+++ setup.sh
@@ -6,8 +6,14 @@ echo "Installing zsh if it wasn't present"
 echo "Changing shell to zsh"
 chsh -s /usr/bin/zsh
 
+echo "Copying oh my zsh files"
+cp -rv .oh-my-zsh $(HOME)/.oh-my-zsh
+
+echo "Replacing .p10k.zsh"
+cp -v .p10k.zsh $(HOME)/.p10k.zsh
+
 echo "Replacing .zshrc"
-cp -v .zshrc ~/.zshrc
+cp -v .zshrc $(HOME)/.zshrc
 
 echo "Replacing .gitconfig"
-cp -v .gitconfig ~/.gitconfig
+cp -v .gitconfig $(HOME)/.gitconfig
